ad6f2babb1775cbc1e8ad9a73b92e8ae47e016ff
genautkin <39458033+genautkin@users.noreply.github.com>
2021-07-04 20:50:01 +0300

    First comit

---
+++ index.js
@@ -0,0 +1,20 @@
+//set express
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+//set static folder
+const path = require("path");
+const publicDirectoryPath = path.join(__dirname, "./public");
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+
+    res.render("index");
+    });
+    
+
+app.listen(port, () => {
+  console.log('Server started on port:'+port)
+})
